57a590b1deb97d5c846a21a3c42125c71b08b8d1
kapil kumar <kapilkd13@gmail.com>
2017-05-31 19:10:30 +0530

    adding / path separator and removing on file opening in fetch text func

--- schema_salad/ref_resolver.py
+++ schema_salad/ref_resolver.py
@@ -148,6 +148,11 @@ class DefaultFetcher(Fetcher):
             try:
                 print("separated path below")
                 print(path)
+                # On Windows, url.path will be /drive:/path ; on Unix systems,
+                # /path. As we want drive:/path instead of /drive:/path on Windows,
+                # remove the leading /.
+                if os.path.isabs(path[1:]):
+                    path = path[1:]
                 with open(urllib.url2pathname(str(path))) as fp:
                     read = fp.read()
                 if hasattr(read, "decode"):
